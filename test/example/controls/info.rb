bfabe1c232b0293b57e4e2bdd0e83d86be419570
Adham <ahelal@users.noreply.github.com>
2019-04-18 16:40:26 +0200

    remove unused test

--- test/example/controls/info.rb
+++ test/example/controls/info.rb
@@ -4,6 +4,5 @@ control 'Check opsman' do
   title 'should be reachable'
   describe om_info do
     its('version') { should match(/2.3/) }
-    its('last_completed_run') { should match(/2.3/) }
   end
 end
