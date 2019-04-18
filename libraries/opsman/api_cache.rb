b484724e06be37eee7eed520f9555b2d07640aa9
Adham <ahelal@users.noreply.github.com>
2019-04-18 15:07:11 +0200

    add new cahe class

---
+++ libraries/opsman/api_cache.rb
@@ -0,0 +1,37 @@
+
+
+class RequestCache
+  def initialize
+    @cache_time = ENV['INSPEC_CACHE_TIME'] || ''
+    @cache_enabled = !@cache_time.empty?
+    @cache_dir = ENV['INSPEC_CACHE_DIR'] || "#{ENV['HOME']}/.inspec_cache"
+    cache_setup
+  end
+
+  def get_cache(id)
+    return false unless @cache_enabled
+    cache_file_path, cache_exist = check_cache_file(id)
+    return false unless cache_exist
+    diff_seconds = (Time.new - File.stat(cache_file_path).ctime).to_i
+    return false if diff_seconds > @cache_time.to_i
+    JSON.parse(File.open(cache_file_path).read)
+  end
+
+  def write_cache(id, contet)
+    return false unless @cache_enabled
+    cache_file_path = "#{@cache_dir}/#{id}.json"
+    File.write(cache_file_path, contet)
+  end
+
+  private
+
+  def cache_setup
+    return false unless @cache_enabled
+    Dir.mkdir @cache_dir unless File.exist?(@cache_dir)
+  end
+
+  def check_cache_file(id)
+    cache_file_path = "#{@cache_dir}/#{id}.json"
+    [cache_file_path, File.file?(cache_file_path)]
+  end
+end
