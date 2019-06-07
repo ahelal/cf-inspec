5521a58e0dfa51402d38cc4c5ae6fc2fb435ecc6
Philip Kuryloski <pkuryloski@pivotal.io>
2019-06-07 09:55:44 +0200

    Faraday problems and fixes 2

--- libraries/bosh/bosh_client.rb
+++ libraries/bosh/bosh_client.rb
@@ -35,9 +35,9 @@ class BoshClient
   def bosh_api
     conn = Faraday.new(url: bosh_director_url, ssl: { ca_file: @ca_path }) do |f|
       f.use FaradayMiddleware::FollowRedirects, limit: 3
-      f.authorization :Bearer, access_token
-      f.adapter Faraday.default_adapter
+      f.adapter :net_http
     end
+    conn.authorization :Bearer, access_token
     conn.port = 25_555
     conn
   end
